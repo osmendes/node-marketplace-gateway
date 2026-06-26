10ce04758f046882bfde6e79ec4f1de362965437
Maycon Mendes <maycon.mendes@quallecontrol.com.br>
2026-06-26 16:49:19 -0300

    feat(circuit-breaker): cria módulo de circuit breaker e métodos de execução

--- src/proxy/proxy.module.ts
+++ src/proxy/proxy.module.ts
@@ -1,9 +1,10 @@
 import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
+import { CircuitBreakerModule } from "src/common/circuit-breaker/circuit-breaker.module";
 import { ProxyService } from "./service/proxy.service";
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CircuitBreakerModule],
   providers: [ProxyService],
   exports: [ProxyService],
 })
